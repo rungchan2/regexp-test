95b64af233e6563cd681839db6535474b7ddf9f8
rungchan2 <leeh09077@gmail.com>
2022-07-03 18:35:39 +0900

    create project

---
+++ main.js
@@ -0,0 +1,13 @@
+const str = `
+010-1234-5678
+thethe@gmail.com
+https://naver.com
+this is such a hard task
+aaaaasssssdddd
+동해불과_백두산이 마르고 닳도록
+`
+
+// const regexp = new RegExp('the', 'gi')
+console.log (
+  str.match(/\w/g)
+)
